c55942ee44367f66a72f34006eebccf2ac838ffb
Denis XP <xpachin@gmail.com>
2022-07-21 13:57:29 -0400

    modulo 8clase 5

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const express=require('express');
 const {conexion}=require("./bd");
 const fs=require("fs");
+const upload=require("express-fileupload");
+const path=require("path");
 
 //incializacion
 const app=new express();
 app.use(express.static("public"));
+app.use(upload());
 app.set("view engine","ejs");
 app.set("views",__dirname+"/views");
 
@@ -50,6 +53,70 @@ app.get('/galeria', async (req, res) => {
 app.get('/contacto', (req, res) => {
   res.render('contacto');
 })
+
+//-------------------------------------------
+app.get("/ingresolibros",async function(req,res){
+const consultaAutores='SELECT "Id","Nombre" FROM "Autores"';
+const consultaGeneros='SELECT "Id","Nombre" FROM "Genero"'
+const consultaEditoriales='SELECT "Id","Nombre" FROM "Editorial"'
+const consultaIdiomas='SELECT "Id","Nombre" FROM "Idioma"'
+let respuestaAutores;
+let respuestaGeneros;
+let respuestaEditoriales;
+let respuestaIdiomas;
+try {
+   respuestaAutores=await conexion.query(consultaAutores);  
+   respuestaGeneros=await conexion.query(consultaGeneros);  
+   respuestaEditoriales=await conexion.query(consultaEditoriales);  
+   respuestaIdiomas=await conexion.query(consultaIdiomas);  
+} catch (error) {
+  console.log("error consulta:"+error.message)
+}
+const autores=respuestaAutores.rows;
+const generos=respuestaGeneros.rows;
+const editoriales=respuestaEditoriales.rows;
+const idiomas=respuestaIdiomas.rows;
+
+console.log(autores);
+res.render("ingresoLibros",{autores,generos,editoriales,idiomas})
+
+});
+
+
+app.post("/agregarLibro",async function(req,res){
+  //buscar el id para el nuevo libro
+  consultaId='SELECT COALESCE(MAX("Id"),0)+1 AS "Id" FROM "Libros"';
+  let respuesta;
+  try {
+    respuesta=await conexion.query(consultaId);
+  } catch (error) {
+    console.log("error consulta:"+error.message)
+    return res.status(500).send("Error al insertar datos");
+  }
+  console.log(respuesta);
+  const id=respuesta.rows[0].Id;
+
+  //agregar los datos a la BD
+  consultaInsert='INSERT INTO "Libros" VALUES($1,$2,$3,$4,$5,$6,$7,$8)'
+  const parametros=[id,req.body.nombre,req.body.paginas,req.body.edicion,req.body.autor,req.body.editorial,req.body.genero,req.body.idioma];
+  try {
+    await conexion.query(consultaInsert,parametros);
+  } catch (error) {
+    console.log("error consulta:"+error.message)
+    return res.status(500).send("Error al insertar datos");
+  }
+  //guardar foto
+  const ruta=__dirname+"/public/img/"+id+"."+path.extname(req.files.imagen.name);
+  req.files.imagen.mv(ruta,function(err){
+    if(err){
+      console.log("error al guardar archivo:"+error.message)
+      return res.status(500).send("Error al guardar archivo");
+    }
+  })
+
+  res.send("OK");
+});
+
 
 
 module.exports={app}
